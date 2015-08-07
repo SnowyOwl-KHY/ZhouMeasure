3fb06047304d20c6e46779e7ccd06308c0246a7d
liyansam <liyansam@qq.com>
2015-08-07 13:33:53 +0800

    add photoView package

---
+++ app/src/main/java/classexample/zhoumeasure/photoView/DrawArea.java
@@ -0,0 +1,16 @@
+package classexample.zhoumeasure.photoView;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by liyan on 8/7/15.
+ */
+public class DrawArea extends View {
+
+    public DrawArea(Context context) {
+        super(context);
+    }
+
+
+}
