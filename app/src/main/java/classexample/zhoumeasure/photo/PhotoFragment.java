fee79c58135f87059862da2372c3dab700bc9f06
silrep <father.leftchild@gmail.com>
2015-08-07 14:35:34 +0800

    empty file

---
+++ app/src/main/java/classexample/zhoumeasure/photo/PhotoFragment.java
@@ -0,0 +1,22 @@
+package classexample.zhoumeasure.photo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Xiaozhou on 2015/8/7.
+ */
+public class PhotoFragment extends Fragment{
+    View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //rootView = inflater.inflate(yourXML, container, false);
+        return rootView;
+    }
+}
