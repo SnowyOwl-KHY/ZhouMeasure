67bb2b9d1bdd85b62651e64c2b244d08785c9d25
silrep <father.leftchild@gmail.com>
2015-08-07 15:42:48 +0800

    runnable

--- app/src/main/java/classexample/zhoumeasure/photo/PhotoFragment.java
+++ app/src/main/java/classexample/zhoumeasure/photo/PhotoFragment.java
@@ -1,11 +1,14 @@
 package classexample.zhoumeasure.photo;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 import classexample.zhoumeasure.R;
 
@@ -19,6 +22,10 @@ public class PhotoFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.photo_layout, container, false);
+        ProgressWheel wheel = new ProgressWheel(container.getContext());
+        wheel.setBarColor(Color.BLUE);
+
+        container.addView(wheel);
         return rootView;
     }
 }
